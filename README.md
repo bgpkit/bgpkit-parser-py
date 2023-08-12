31baeb26aafcc38c0aef86b5851fe39681815b68
Mingwei Zhang <mingwei@mwzhang.com>
2023-08-12 09:52:21 -0700

    migrate submodule back

--- README.md
+++ README.md
@@ -5,11 +5,32 @@ Python binding for bgpkit-parser
 ## Example
 
 ```python
-
 from pybgpkit_parser import Parser
 import json
 
-parser = Parser(url="https://spaces.bgpkit.org/parser/update-example", filters={"peer_ips": "185.1.8.65, 2001:7f8:73:0:3:fa4:0:1"})
+parser = Parser(
+    url="https://spaces.bgpkit.org/parser/update-example",
+    filters={"peer_ips": "185.1.8.65, 2001:7f8:73:0:3:fa4:0:1"},
+)
+
+for elem in parser:
+    print(elem["origin_asns"])
+    print(json.dumps(elem, indent=4))
+    break
+```
+
+You can also add `cache_dir` to Parser to cache the downloaded files to a specified directory.
+
+Here is an example:
+```python
+from pybgpkit_parser import Parser
+import json
+
+parser = Parser(
+    url="https://spaces.bgpkit.org/parser/update-example",
+    filters={"peer_ips": "185.1.8.65, 2001:7f8:73:0:3:fa4:0:1"},
+    cache_dir="./"
+)
 
 for elem in parser:
     print(elem["origin_asns"])
@@ -53,8 +74,47 @@ maturin publish --interpreter python3.10 --skip-existing
 maturin publish --interpreter python3.11 --skip-existing
 ```
 
+#### Using docker
+
+```
+docker build -t bgpkit-builder:latest .
+docker run --rm -it bgpkit-builder:latest bash
+
+####
+# TODO: copy the content of .pypirc to the root folder
+####
+git clone https://github.com/bgpkit/bgpkit-parser.git
+cd bgpkit-parser/bgpkit-parser-py
+
+maturin publish --interpreter python3.7 --skip-existing
+maturin publish --interpreter python3.8 --skip-existing
+maturin publish --interpreter python3.9 --skip-existing
+maturin publish --interpreter python3.10 --skip-existing
+maturin publish --interpreter python3.11 --skip-existing
+
+```
+
 ### Publish for MacOS
 
+#### M1-based
+
+**Minimum support version for M1 Macs is Python 3.8**
+
+Install multiple Python interpreters:
 ```bash
-maturin publish --skip-existing
+brew install python@3.8
+brew install python@3.9
+brew install python@3.10
+brew install python@3.11
 ```
+
+```bash
+maturin publish --interpreter python3.8 --skip-existing
+maturin publish --interpreter python3.9 --skip-existing
+maturin publish --interpreter python3.10 --skip-existing
+maturin publish --interpreter python3.11 --skip-existing
+```
+
+#### Intel-based
+
+- [ ] add support for packaging for Intel-based Macs 
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,43 @@
-use std::collections::HashMap;
+use bgpkit_parser::models::*;
 use bgpkit_parser::*;
+use dict_derive::{FromPyObject, IntoPyObject};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
-use dict_derive::{FromPyObject, IntoPyObject};
+use std::collections::HashMap;
+use std::io::Read;
 
 #[pymodule]
 fn pybgpkit_parser(_py: Python, m: &PyModule) -> PyResult<()> {
-
     fn convert_elem(elem: BgpElem) -> Elem {
         Elem {
             timestamp: elem.timestamp,
             elem_type: match elem.elem_type {
-                ElemType::ANNOUNCE => {"A".to_string()}
-                ElemType::WITHDRAW => {"W".to_string()}
-            }
-            ,
+                ElemType::ANNOUNCE => "A".to_string(),
+                ElemType::WITHDRAW => "W".to_string(),
+            },
             peer_ip: elem.peer_ip.to_string(),
             peer_asn: elem.peer_asn.asn,
             prefix: elem.prefix.to_string(),
             next_hop: elem.next_hop.map(|v| v.to_string()),
             as_path: elem.as_path.map(|v| v.to_string()),
-            origin_asns: elem.origin_asns.map(|v| v.into_iter().map(|x|x.asn).collect()),
+            origin_asns: elem
+                .origin_asns
+                .map(|v| v.into_iter().map(|x| x.asn).collect()),
             origin: elem.origin.map(|v| v.to_string()),
             local_pref: elem.local_pref,
             med: elem.med,
-            communities: elem.communities.map(|v| v.into_iter().map(|x|x.to_string()).collect()),
+            communities: elem
+                .communities
+                .map(|v| v.into_iter().map(|x| x.to_string()).collect()),
             atomic: match elem.atomic {
-                None => {
-                    None
-                }
-                Some(v) => {match v {
-                    AtomicAggregate::NAG => {Some("NAG".to_string())}
-                    AtomicAggregate::AG => {Some("AG".to_string())}
-                }}
+                None => None,
+                Some(v) => match v {
+                    AtomicAggregate::NAG => Some("NAG".to_string()),
+                    AtomicAggregate::AG => Some("AG".to_string()),
+                },
             },
             aggr_asn: elem.aggr_asn.map(|v| v.asn),
-            aggr_ip: elem.aggr_ip.map(|v| v.to_string())
+            aggr_ip: elem.aggr_ip.map(|v| v.to_string()),
         }
     }
 
@@ -55,33 +57,38 @@ fn pybgpkit_parser(_py: Python, m: &PyModule) -> PyResult<()> {
         pub communities: Option<Vec<String>>,
         pub atomic: Option<String>,
         pub aggr_asn: Option<u32>,
-        pub aggr_ip: Option<String>
+        pub aggr_ip: Option<String>,
     }
 
     #[pyclass]
     #[pyo3(text_signature = "(url, filters, /)")]
     struct Parser {
-        elem_iter: ElemIterator,
+        elem_iter: ElemIterator<Box<dyn Send + Read>>,
     }
 
     #[pymethods]
     impl Parser {
         #[new]
-        fn new(url: String, filters: Option<HashMap<String, String>>) -> PyResult<Self> {
-            let mut parser = BgpkitParser::new(url.as_str()).unwrap();
+        fn new(
+            url: String,
+            filters: Option<HashMap<String, String>>,
+            cache_dir: Option<String>,
+        ) -> PyResult<Self> {
+            let mut parser = match cache_dir {
+                None => BgpkitParser::new(url.as_str()).unwrap(),
+                Some(dir) => BgpkitParser::new_cached(url.as_str(), dir.as_str()).unwrap(),
+            };
 
             if let Some(filters) = filters {
-                for (k,v) in filters {
-                   parser = match parser.add_filter(k.as_str(), v.as_str()) {
-                       Ok(p) => {p}
-                       Err(e) => {
-                           return Err(PyValueError::new_err(e.to_string()))
-                       }
-                   }
+                for (k, v) in filters {
+                    parser = match parser.add_filter(k.as_str(), v.as_str()) {
+                        Ok(p) => p,
+                        Err(e) => return Err(PyValueError::new_err(e.to_string())),
+                    }
                 }
             }
             let elem_iter = parser.into_iter();
-            Ok(Parser{elem_iter})
+            Ok(Parser { elem_iter })
         }
 
         fn parse_all(&mut self) -> PyResult<Vec<Elem>> {
@@ -89,10 +96,8 @@ fn pybgpkit_parser(_py: Python, m: &PyModule) -> PyResult<()> {
 
             loop {
                 match self.elem_iter.next() {
-                    None => {break}
-                    Some(e) => {
-                        elems.push(convert_elem(e))
-                    }
+                    None => break,
+                    Some(e) => elems.push(convert_elem(e)),
                 }
             }
             Ok(elems)
